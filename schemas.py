7dccbb361d810d897e365f88f174e0cb45212192
Subhansh-1-u <iamsubhiitian1@gmail.com>
2025-11-15 22:41:42 +0530

    Add Post, Gallery, and Event models
    Added models for Post, Gallery, and Event with appropriate fields and configurations.

--- schemas.py
+++ schemas.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel, EmailStr
-
+from datetime import datetime
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
-   
 
 class UserLogin(BaseModel):
     username: str
@@ -18,3 +17,50 @@ class ShowUser(BaseModel):
 
     class Config:
         orm_mode = True
+        from_attributes = True        
+
+#---------------------------------
+class PostBase(BaseModel):
+     title: str
+     content: str
+     username: str
+     image: str | None = None
+
+class PostCreate(PostBase):
+     pass
+
+class ShowPost(PostBase):
+     id: int
+     date: datetime
+
+     class Config:
+        from_attributes = True
+
+
+class GalleryBase(BaseModel):
+     src: str
+     caption: str
+
+class GalleryCreate(GalleryBase):
+     pass
+
+class ShowGallery(GalleryBase):
+     id: int
+
+     class Config:
+         from_attributes = True
+
+
+class EventBase(BaseModel):
+    name: str
+    date: str
+    location: str
+
+class EventCreate(EventBase):
+     pass
+
+class ShowEvent(EventBase):
+    id: int
+
+    class Config:
+        from_attributes = True

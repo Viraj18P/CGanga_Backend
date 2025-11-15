397a21a65927279f7a29b3220ea6bb555f1b43ff
Subhansh-1-u <iamsubhiitian1@gmail.com>
2025-11-15 22:42:25 +0530

    Add Post, GalleryItem, and Event models

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String,Boolean
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean
 from database import Base
-
+from datetime import datetime
 class User(Base):
     __tablename__ = "users"
 
@@ -10,3 +10,27 @@ class User(Base):
     hashed_password = Column(String)
     is_verified = Column(Boolean, default=False)          
     verification_token = Column(String, nullable=True) 
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(Text)
+    username = Column(String)
+    image = Column(Text, nullable=True)    
+    date = Column(DateTime, default=datetime.utcnow)
+
+class GalleryItem(Base):
+    __tablename__ = "gallery_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    src = Column(Text)   
+    caption = Column(String)
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    date = Column(String)  
+    location = Column(String)
